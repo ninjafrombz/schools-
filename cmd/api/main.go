a6fe50d2b755598d0c5e1f11f4dee47f1a70cd31
Desire Amagwula <dezamagwula@gmail.com>
2022-08-22 13:32:33 -0600

    First commit of project setup

---
+++ cmd/api/main.go
@@ -0,0 +1,13 @@
+// take note of the name and location of the file on the first line
+// Filename: cmd/api/main.go 
+
+package main
+
+import (
+	"fmt"
+
+)
+
+func main () {
+	fmt.Println(" My name is Jeffwardo ")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module schools.desireamagwula.net
+
+go 1.19
